c4284c70eb657f99663fc1eecd938ff418bf7651
Ivan Krastev <igkrastev@gmail.com>
2025-02-12 02:42:34 +0200

    Updated code

--- define_class.py
+++ define_class.py
@@ -2,7 +2,7 @@
 
 class BankAccount:
     def __init__(self, name, balance, loan, transaction_history = None):
-        self.account_holder = name # Account names
-        self.balance = balance # Account balances
+        self.account_holder = name # Account name
+        self.balance = balance # Account balance
         self.loan = loan # Account loan details
         self.transaction_history = transaction_history if transaction_history is not None else [] # Account transaction logs
